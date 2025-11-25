928a35dd07ad7ce80b7973cbc91bec5625933918
Léo Moille <leomoille18@gmail.com>
2025-11-25 16:27:38 +0100

    refactor(game): apply OCP with separate evaluators for high and low score rules

--- src/main/java/games/Game.java
+++ src/main/java/games/Game.java
@@ -6,7 +6,7 @@ import view.View;
 
 public class Game {
     public static void main(String[] args) {
-        GameController gc = new GameController(new Deck(), new View(), new GameEvaluator());
+        GameController gc = new GameController(new Deck(), new View(), new LowCardGameEvaluator());
         gc.run();
     }
 }
