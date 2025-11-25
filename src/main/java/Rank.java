2aef6c0a581b7a6532eb8308c5cbcefa243c17dd
Léo Moille <leomoille18@gmail.com>
2025-11-25 11:54:03 +0100

    chore(model): add Rank and Suit enums

---
+++ src/main/java/Rank.java
@@ -0,0 +1,23 @@
+public enum Rank {
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    JACK(10),
+    QUEEN(11),
+    KING(12),
+    ACE(13);
+
+    final int rank;
+
+    private Rank(int value) {
+        this.rank = value;
+    }
+
+    public int value() {
+        return this.rank;
+    }
+}
---
+++ src/main/java/Suit.java
@@ -0,0 +1,13 @@
+public enum Suit {
+    DIAMONDS(1), HEART(2), SPADES(3), CLUBS(4);
+
+    final int suit;
+
+    private Suit(int value) {
+        this.suit = value;
+    }
+
+    public int value() {
+        return this.suit;
+    }
+}
