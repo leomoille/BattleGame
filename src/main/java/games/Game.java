0119dc2c10643d9e8b52aaa252d9ab156655050f
Léo Moille <leomoille18@gmail.com>
2025-11-26 09:18:12 +0100

    feat(ui): add Swing-based GameSwingView and integrate it into game startup
    - Introduce GameSwingView as new GUI implementation
    - Update Game.java to use GameSwingView instead of CommandLineView
    - Add JavaFX Swing dependency in pom.xml for UI support

--- src/main/java/games/Game.java
+++ src/main/java/games/Game.java
@@ -2,11 +2,17 @@ package games;
 
 import controller.GameController;
 import model.Deck;
-import view.CommandLineView;
+// import view.CommandLineView;
+import view.GameSwingView;
 
 public class Game {
     public static void main(String[] args) {
-        GameController gc = new GameController(new Deck(), new CommandLineView(), new LowCardGameEvaluator());
+        // CommandLineView clv = new CommandLineView();
+        GameSwingView gsv = new GameSwingView();
+        gsv.createAndShowGUI();
+
+        GameController gc = new GameController(new Deck(), gsv, new LowCardGameEvaluator());
+
         gc.run();
     }
 }
